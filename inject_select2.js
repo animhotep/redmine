79da8913c1f188ae505c8a5f5368be044e4900b1
animhotep <animhotep@gmail.com>
2025-09-25 12:15:27 +0300

    scroll

---
+++ inject_select2.js
@@ -0,0 +1,7 @@
+(() => {
+  $('#issue_assigned_to_id').select2().on('select2:open', function () {
+    setTimeout(function () {
+      $('.select2-results__options').scrollTop(0);
+    },40)
+  });
+})();
